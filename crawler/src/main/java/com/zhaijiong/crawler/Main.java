d98c329c7a215235ac0dd232e6e05995a67939ab
eryk <xuqi86@gmail.com>
2014-08-12 17:54:48 +0800

    修改url是否重复

--- crawler/src/main/java/com/zhaijiong/crawler/Main.java
+++ crawler/src/main/java/com/zhaijiong/crawler/Main.java
@@ -4,14 +4,17 @@ import com.google.common.base.Preconditions;
 import com.zhaijiong.crawler.dao.ReportTemplate;
 import com.zhaijiong.crawler.pipeline.MongoDBPipeline;
 import com.zhaijiong.crawler.processor.BaseReportProcessor;
+import com.zhaijiong.crawler.scheduler.RedisDuplicateRemover;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.scheduler.QueueScheduler;
 
 /**
  * Created by xuqi.xq on 2014/8/12.
@@ -36,10 +39,17 @@ public class Main implements CommandLineRunner{
         Preconditions.checkNotNull(template,"build repost template fail.please check path and file.");
 
         BaseReportProcessor processor = new BaseReportProcessor(template,site);
+
+        QueueScheduler scheduler = new QueueScheduler();
+
+        JedisPool pool = new JedisPool("localhost",6379);
+        scheduler.setDuplicateRemover(new RedisDuplicateRemover(template,pool,1000000));
+
         Spider.create(processor)
                 .addUrl(template.getSeedUrl())
                 .thread(5)
                 .addPipeline(pipeline)
+                .setScheduler(scheduler)
                 .run();
     }
 }
