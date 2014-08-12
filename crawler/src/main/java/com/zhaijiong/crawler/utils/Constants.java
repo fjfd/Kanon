ffb3e98565b2be50063e4299f34d99f9622c6249
eryk <xuqi86@gmail.com>
2014-08-12 17:53:58 +0800

    update

--- crawler/src/main/java/com/zhaijiong/crawler/utils/Constants.java
+++ crawler/src/main/java/com/zhaijiong/crawler/utils/Constants.java
@@ -4,11 +4,13 @@ package com.zhaijiong.crawler.utils;
  * Created by xuqi86@gmail.com on 2014/8/10.
  */
 public class Constants {
+    public static final String PROJECT = "kanon_";
+
     public static final String ALGORITHM = "MD5";
     public static final String ENCODING = "utf-8";
 
     public static final String SEED_URL ="seedUrl";
     public static final String POST_PAGE_URL_RULE ="postPageUrlRule";
-    public static final String LIST_PAGE_URL_RULE ="postPageUrlRule";
+    public static final String LIST_PAGE_URL_RULE ="listPageUrlRule";
 
 }
